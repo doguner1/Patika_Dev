00ca97a6f071293f56a2e0997faf307ff5997fd0
doguner1 <d.gunr@yaani.com>
2023-05-06 12:27:14 +0300

    first commit

--- DersOdevleri/HipotenusBulma.java
+++ DersOdevleri/HipotenusBulma.java
@@ -27,3 +27,68 @@ public class HipotenusBulma {
         double alan = Math.sqrt(u * (u - a) * (u - b) * (u - hipotenus));
     }
 }
+
+
+public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    
+    int distance, age, travelType;
+    double basePrice, ageDiscount, discountedPrice, roundTripDiscount, totalPrice;
+    
+    // Only accept positive values for distance
+    do {
+        System.out.print("Enter distance in km: ");
+        distance = input.nextInt();
+        if (distance < 0) {
+            System.out.println("Invalid input!");
+        }
+    } while (distance < 0);
+    
+    // Only accept positive values for age
+    do {
+        System.out.print("Enter your age: ");
+        age = input.nextInt();
+        if (age < 0) {
+            System.out.println("Invalid input!");
+        }
+    } while (age < 0);
+    
+    // Only accept 1 or 2 for travel type
+    do {
+        System.out.print("Enter travel type (1 for one-way, 2 for round-trip): ");
+        travelType = input.nextInt();
+        if (travelType != 1 && travelType != 2) {
+            System.out.println("Invalid input!");
+        }
+    } while (travelType != 1 && travelType != 2);
+    
+    // Calculate base price
+    basePrice = distance * 0.10;
+    
+    // Calculate age discount
+    if (age < 12) {
+        ageDiscount = basePrice * 0.50;
+    } else if (age >= 12 && age <= 24) {
+        ageDiscount = basePrice * 0.10;
+    } else if (age >= 65) {
+        ageDiscount = basePrice * 0.30;
+    } else {
+        ageDiscount = 0;
+    }
+    
+    // Calculate discounted price
+    discountedPrice = basePrice - ageDiscount;
+    
+    // Calculate round-trip discount
+    if (travelType == 2) {
+        roundTripDiscount = discountedPrice * 0.20;
+    } else {
+        roundTripDiscount = 0;
+    }
+    
+    // Calculate total price
+    totalPrice = (discountedPrice - roundTripDiscount) * travelType;
+    
+    System.out.println("Total price = " + totalPrice + " TL");
+}
